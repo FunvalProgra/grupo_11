2a01c2d936eeda5847b1126bb86363ced0f42a06
IsaiasZc <coroxerox2@gmail.com>
2023-09-01 14:59:12 -0500

    dia 10 - tarde

---
+++ dia10/debugging/debug.js
@@ -0,0 +1,15 @@
+const list1 = document.querySelector("#list1");
+const list2 = document.querySelector("#list2");
+const list3 = document.querySelector("#list3");
+const list4 = document.querySelector("#list4");
+
+
+list1.textContent = "Hola Mundo";
+list2.textContent = "Hola Mundo";
+list3.textContent = "Hola Mundo";
+list4.textContent = "Hola Mundo";
+
+// error de sintáxis
+
+
+// error de lógica
