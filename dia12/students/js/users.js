ddf278619f0185ca456c088044e859aa1b9a70b5
IsaiasZc <coroxerox2@gmail.com>
2023-09-05 10:52:10 -0500

    before login users

---
+++ dia12/students/js/users.js
@@ -0,0 +1,92 @@
+const users = [
+  {
+    id: 1,
+    first_name: "Howey",
+    last_name: "Scarisbrick",
+    email: "hscarisbrick0@aboutads.info",
+    gender: "Male",
+    password: "aK0}|T=JX\"E@9q{",
+    tel: "553-651-5919"
+  },
+  {
+    id: 2,
+    first_name: "Hagan",
+    last_name: "Humby",
+    email: "hhumby1@example.com",
+    gender: "Male",
+    password: "sC5|3r9!Hs5",
+    tel: "989-687-6655"
+  },
+  {
+    id: 3,
+    first_name: "Karel",
+    last_name: "Collen",
+    email: "kcollen2@ezinearticles.com",
+    gender: "Female",
+    password: "pD0?}NcqL",
+    tel: "254-157-0339"
+  },
+  {
+    id: 4,
+    first_name: "Conrado",
+    last_name: "Bayles",
+    email: "cbayles3@who.int",
+    gender: "Male",
+    password: "iB0(Ls%*,<",
+    tel: "251-697-9936"
+  },
+  {
+    id: 5,
+    first_name: "Wayland",
+    last_name: "Huertas",
+    email: "whuertas4@alexa.com",
+    gender: "Male",
+    password: "xT3&t(B0P1{(Wy$K",
+    tel: "669-813-6909"
+  },
+  {
+    id: 6,
+    first_name: "Marsiella",
+    last_name: "Tackell",
+    email: "mtackell5@usda.gov",
+    gender: "Female",
+    password: "oQ6<!2nB@`b5Ze",
+    tel: "643-243-5260"
+  },
+  {
+    id: 7,
+    first_name: "Christopher",
+    last_name: "Deare",
+    email: "cdeare6@alexa.com",
+    gender: "Male",
+    password: "sL6%VI4EU#@@",
+    tel: "347-597-6139"
+  },
+  {
+    id: 8,
+    first_name: "Cristine",
+    last_name: "Greenstock",
+    email: "cgreenstock7@goodreads.com",
+    gender: "Female",
+    password: "jZ9=%QJ`Kp",
+    tel: "243-971-5244"
+  },
+  {
+    id: 9,
+    first_name: "Ware",
+    last_name: "Swallow",
+    email: "wswallow8@wikimedia.org",
+    gender: "Male",
+    password: "hZ0\\V.|s=d",
+    tel: "297-591-3518"
+  },
+  {
+    id: 10,
+    first_name: "Thorvald",
+    last_name: "Janak",
+    email: "tjanak9@friendfeed.com",
+    gender: "Male",
+    password: "oB5?Oig<BTYYm0HX",
+    tel: "649-755-7597"
+  }
+]
