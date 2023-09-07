7af5531cf7d56956641cc2e3bd787444119f2437
IsaiasZc <coroxerox2@gmail.com>
2023-09-07 10:27:29 -0500

    dia 14 mañana 2

---
+++ dia14/students/js/functions.js
@@ -0,0 +1,21 @@
+
+function crearRow(student) {
+  let tr = `
+  <tr> 
+    <td>${student.matricula}</td>
+    <td>${student.nombre}</td>
+    <td>${student.responsable}</td>
+    <td>${student.pais}</td>
+    <td><button data-id="${student.id}" class="delete">X</button></td>
+  </tr>
+  `
+
+  return tr
+}
+
+export function print(array, donde) {
+
+  donde.innerHTML = "";
+
+  array.forEach( student =>  donde.innerHTML += crearRow(student))
+}
